9bc1ea80fc9285aab4039965b7153284e026cd76
MoQa-atx <omerersuva1@gmail.com>
2025-10-28 23:53:04 +0300

    Time: 0 ms (100.00%) | Memory: 8.5 MB (31.64%) - LeetSync

--- 7-reverse-integer/reverse-integer.c
+++ 7-reverse-integer/reverse-integer.c
@@ -1,33 +1,13 @@
 int reverse(int x) {
-    char b[20];
-    int depo[20];
-    int a = 0;
+    long result = 0;  // long overflow için
 
-    long val = x;  // long yapıyoruz
-    int isNegative = 0;
-
-    if (val < 0) {
-        isNegative = 1;
-        val = -val;  // artık güvenli
+    while (x != 0) {
+        result = result * 10 + x % 10;
+        x /= 10;
     }
 
-    sprintf(b, "%ld", val);
-
-    for (int i = 0; b[i] != '\0'; i++) {
-        if (b[i] >= '0' && b[i] <= '9') {
-            depo[a] = b[i] - '0';
-            a++;
-        }
-    }
-
-    long result = 0;
-    for (int i = a - 1; i >= 0; i--) {
-        result = result * 10 + depo[i];
-    }
-
-    if (isNegative) result = -result;
-
-    if (result > 2147483647 || result < -2147483648) return 0;
+    if (result > 2147483647 || result < -2147483648)
+        return 0;
 
     return (int)result;
 }
